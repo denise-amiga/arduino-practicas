28287aa997cf649236c5a8f9533bd1d5b6a7d1ca
denise <denise@amiga.com>
2017-04-19 18:53:59 +0200

    update debug

--- c_centralizado_iluminacion/src/main.cpp
+++ c_centralizado_iluminacion/src/main.cpp
@@ -2,22 +2,22 @@
 
 #include "myleds.h"
 
-#define pulsador_conmutador                     4
+#define pulsador_conmutador             4
 #define pulsador_temporizado            5
-#define pulsador_zumbador                       6
-#define salida_conmutada                        10
-#define salida_temporizada                      11
-#define salida_zumbador                         12
+#define pulsador_zumbador               6
+#define salida_conmutada               10
+#define salida_temporizada             11
+#define salida_zumbador                12
 
-#define tiempo_temporizador                     20
-#define tiempo_zumbador1                        5
-#define tiempo_zumbador2                        15
+#define tiempo_temporizador            20
+#define tiempo_zumbador1                5
+#define tiempo_zumbador2               15
 
 int pin4_status;
 int tiempo1;
 int tiempo2;
 int tiempo3;
-boolean zumbador_status = false;
+bool zumbador_status = false;
 
 void setup()
 {
@@ -42,11 +42,11 @@ void loop()
 		digitalWrite(salida_conmutada, !digitalRead(salida_conmutada));
 		Serialprintf("Conmutador: %s\n", digitalRead(salida_conmutada) ? "On" : "Off");
 	}
-
 	pin4_status = digitalRead(pulsador_conmutador);
 
 	// pulsador_temporizado
 	if (digitalRead(pulsador_temporizado) == LOW) {
+		Serialprintf("Temporizador: On\n");
 		digitalWrite(salida_temporizada, HIGH);
 		tiempo1 = tiempo_temporizador;
 	}
@@ -61,6 +61,7 @@ void loop()
 	if ((digitalRead(pulsador_zumbador) == LOW) && (digitalRead(salida_zumbador) == LOW) && (zumbador_status == false)) {
 		tiempo2 = tiempo_zumbador1;     // tiempo sonando
 		tiempo3 = tiempo_zumbador2;     // tiempo para poder volver a pulsar
+		Serialprintf("Temporizador-Zumbador: On\n");
 		digitalWrite(salida_zumbador, HIGH);
 		zumbador_status = true;
 	}
@@ -76,5 +77,6 @@ void loop()
 	} else {
 		zumbador_status = false;
 	}
+
 	delay(1000);
 }
