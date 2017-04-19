df6055510dda9332c40617439b9abd29ccdc92d1
denise <denise@amiga.com>
2017-04-19 18:32:44 +0200

    buzzer add

--- c_centralizado_iluminacion/src/main.cpp
+++ c_centralizado_iluminacion/src/main.cpp
@@ -2,14 +2,28 @@
 
 #include "myleds.h"
 
+#define pulsador_conmutador                     4
+#define pulsador_temporizado            5
+#define pulsador_zumbador                       6
+#define salida_conmutada                        10
+#define salida_temporizada                      11
+#define salida_zumbador                         12
+
+#define tiempo_temporizador                     20
+#define tiempo_zumbador1                        5
+#define tiempo_zumbador2                        15
+
 int pin4_status;
-int tiempo;
+int tiempo1;
+int tiempo2;
+int tiempo3;
+boolean zumbador_status = false;
 
 void setup()
 {
 	Serial.begin(38400);
 
-	for (size_t i = 4; i <= 9; i++) {
+	for (int i = 4; i <= 9; i++) {
 		pinMode(i, INPUT_PULLUP);
 		pinMode(i + 6, OUTPUT);
 		digitalWrite(i + 6, LOW);
@@ -23,19 +37,44 @@ void setup()
 
 void loop()
 {
-	if ((digitalRead(4) == LOW) && (pin4_status == HIGH)) {
-		digitalWrite(10, !digitalRead(10));
-		Serial.println(digitalRead(10) ? "On" : "Off");
+	// pulsador_conmutador
+	if ((digitalRead(pulsador_conmutador) == LOW) && (pin4_status == HIGH)) {
+		digitalWrite(salida_conmutada, !digitalRead(salida_conmutada));
+		Serialprintf("Conmutador: %s\n", digitalRead(salida_conmutada) ? "On" : "Off");
 	}
-	if (digitalRead(5) == LOW)
-		tiempo = 20;
-	if (tiempo > 0) {
-		digitalWrite(11, HIGH);
-		Serial.println(tiempo);
-		tiempo--;
+
+	pin4_status = digitalRead(pulsador_conmutador);
+
+	// pulsador_temporizado
+	if (digitalRead(pulsador_temporizado) == LOW) {
+		digitalWrite(salida_temporizada, HIGH);
+		tiempo1 = tiempo_temporizador;
+	}
+	if (tiempo1 > 0) {
+		Serialprintf("Temporizador: %d\n", tiempo1);
+		tiempo1--;
 	} else {
-		digitalWrite(11, LOW);
+		digitalWrite(salida_temporizada, LOW);
 	}
-	pin4_status = digitalRead(4);
+
+	// pulsador_zumbador
+	if ((digitalRead(pulsador_zumbador) == LOW) && (digitalRead(salida_zumbador) == LOW) && (zumbador_status == false)) {
+		tiempo2 = tiempo_zumbador1;     // tiempo sonando
+		tiempo3 = tiempo_zumbador2;     // tiempo para poder volver a pulsar
+		digitalWrite(salida_zumbador, HIGH);
+		zumbador_status = true;
+	}
+	if (tiempo2 > 0) {
+		Serialprintf("Zumbador Sonando: %d\n", tiempo2);
+		tiempo2--;
+	} else {
+		digitalWrite(salida_zumbador, LOW);
+	}
+	if (tiempo3 > 0) {
+		Serialprintf("Temporizador->Zumbador: %d\n", tiempo3);
+		tiempo3--;
+	} else {
+		zumbador_status = false;
+	}
 	delay(1000);
 }
