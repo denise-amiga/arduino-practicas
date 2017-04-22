fff03c8865c95397e77bdfd393aefdaafad8ba48
denise <denise@amiga.com>
2017-04-22 17:11:52 +0200

    updated antirebote (only run once)

--- test_antirebote/src/main.cpp
+++ test_antirebote/src/main.cpp
@@ -3,6 +3,15 @@
 #define FLANCO_BAJADA(x) ((x ## _antes == LOW) && (x ## _despues == HIGH))
 #define FLANCO_SUBIDA(x) ((x ## _antes == HIGH) && (x ## _despues == LOW))
 
+#define t4_tiempo       15
+#define t5_tiempo       15                              // tiempo antes de poder volver a pulsar
+#define t5_sonando       3                              // tiempo sonando
+#define t5_delta        t5_tiempo - t5_sonando          // calculo intermedio necesario
+
+bool bt4_t_once = false;
+bool bt5_t_once = false;
+bool bt5_s_once = false;
+
 int pin4_antes = HIGH, pin4_despues = HIGH;
 int pin5_antes = HIGH, pin5_despues = HIGH;
 int tiempo4, tiempo5;
@@ -21,20 +30,47 @@ void setup()
 void loop()
 {
 	pin4_antes = digitalRead(4);
-	if (FLANCO_BAJADA(pin4))
-		tiempo4 = 10;
-	pin4_despues = pin4_antes;
-	if (tiempo4 > 0) {
-		Serialprintf("Tiempo4 %d\n", tiempo4);
-		tiempo4--;
+	if (FLANCO_BAJADA(pin4)) {
+		Serialprintf("T4: On\n");
+		bt4_t_once = true;
+		tiempo4 = t4_tiempo;
 	}
+	pin4_despues = pin4_antes;
+	if (bt4_t_once) {
+		if (tiempo4 > 0) {
+			Serialprintf("T4 %d\n", tiempo4);
+			tiempo4--;
+		} else {
+			Serialprintf("T4: Off\n");
+			bt4_t_once = false;
+		}
+	}
+
 	pin5_antes = digitalRead(5);
-	if (FLANCO_BAJADA(pin5) && (tiempo5 == 0))
-		tiempo5 = 10;
+	if (FLANCO_BAJADA(pin5) && (tiempo5 == 0)) {
+		Serialprintf("Tiempo5: On\n");
+		bt5_t_once = true;
+		bt5_s_once = true;
+		tiempo5 = t5_tiempo;
+	}
 	pin5_despues = pin5_antes;
-	if (tiempo5 > 0) {
-		Serialprintf("Tiempo5 %d\n", tiempo5);
-		tiempo5--;
+
+	if (bt5_s_once) {
+		if (tiempo5 > t5_delta) {
+			Serialprintf("Sonando5: on\n");
+		} else {
+			Serialprintf("Sonando5: off\n");
+			bt5_s_once = false;
+		}
+	}
+	if (bt5_t_once) {
+		if (tiempo5 > 0) {
+			Serialprintf("T5 %d\n", tiempo5);
+			tiempo5--;
+		} else {
+			Serialprintf("T5: Off\n");
+			bt5_t_once = false;
+		}
 	}
 	delay(1000);
 }
