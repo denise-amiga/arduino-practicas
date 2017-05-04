3c4e4de8cfe53c2c809e36d9f674b834454d9191
denise <denise@amiga.com>
2017-05-04 17:25:20 +0200

    updated dmxserial lib for use with mega (use serial1 for internals)

--- dmxserial/src/main.cpp
+++ dmxserial/src/main.cpp
@@ -2,7 +2,7 @@
 #include "DMXSerial.h"
 
 void setup() {
-  Serial1.begin(38400);
+  Serial.begin(38400);
   DMXSerial.maxChannel(16);
   DMXSerial.init(DMXController, 8); //1
 }
@@ -27,6 +27,6 @@ void loop() {
     DMXSerial.write(3, i);
     delay(10);
   }
-  Serial1.print("Tiempo invertido");
-  Serial1.println((millis() - tiempo) / 1000.0, 3);
+  Serial.print("Tiempo invertido: ");
+  Serial.println((millis() - tiempo) / 1000.0, 3);
 }
